817bfadaf190a256f38a8a1af34648d67eca8bbe
Victor J. Rosario V <victorrosario.1890@gmail.com>
2020-05-04 16:26:09 -0400

    Subiendo Archivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Card, CardDeck, Form, CardColumns } from 'react-bootstrap';
+import Moment from 'react-moment';
+import axios from 'axios';
 
-function App() {
+
+function App () {
+
+  const [ lated, setLated ] = useState([]);
+  const [ results, setResults ] = useState([]);
+  const [ searchCountry, setSearchCountry ] = useState('');
+
+  useEffect(() => {
+    axios
+    .all([
+      axios.get('https://corona.lmao.ninja/v2/all'),
+      axios.get('https://corona.lmao.ninja/v2/countries')
+    ])
+    .then( res => {
+      setLated(res[0].data);
+      setResults(res[1].data);
+    })
+  }, []);
+
+  const date = new Date(parseInt(lated.updated));
+  const lastUpdated = date.toString();
+
+  const filterCountry = results.filter( item => {
+    return searchCountry !== "" ? item.country.includes(searchCountry) : item;
+  });
+
+  const countries = filterCountry.map( (data, i) => {
+    return(
+      <Card key={i} bg='light' text='dark' className='text-center'>
+        <Card.Img variant='top' src={data.countryInfo.flag}/>
+        <Card.Body>
+          <Card.Title>{data.country}</Card.Title>
+
+          <Card.Text>Continent: { data.continent }</Card.Text>
+          <Card.Text>Active: { data.active } </Card.Text>
+          <Card.Text>Cases: { data.cases }</Card.Text>
+          <Card.Text>Deaths: { data.deaths }</Card.Text>
+          <Card.Text>Recovered: { data.recovered }</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CardDeck>
+        <Card bg='secondary' text={'white'} className='text-center' style={{margin: '10px'}}>
+          <Card.Body>
+            <Card.Title>Case</Card.Title>
+            <Card.Text>
+              {lated.cases}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Moment>
+              {lastUpdated}
+            </Moment>
+          </Card.Footer>
+        </Card>
+
+        <Card bg='danger' text={'white'} className='text-center' style={{margin: '10px'}}>
+          <Card.Body>
+            <Card.Title>Deaths</Card.Title>
+            <Card.Text>
+              {lated.deaths}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Moment>
+              {lastUpdated}
+            </Moment>
+          </Card.Footer>
+        </Card>
+
+        <Card bg='success' text={'white'} className='text-center' style={{margin: '10px'}}>
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>
+              {lated.recovered}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Moment>
+              {lastUpdated}
+            </Moment>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+
+      <Form>
+        <Form.Group>
+          <Form.Label>Search</Form.Label>
+          <Form.Control onChange={e => setSearchCountry(e.target.value)} type='text' placeholder='Search a country'/>
+        </Form.Group>
+      </Form>
+
+      <CardColumns>
+        {countries}
+      </CardColumns>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'moment-timezone';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
